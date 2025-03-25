145d921da8aa5d2774047d613cfc3d080cafcff6
shayanAdalat <shayan.saleem@adalat.info>
2025-03-25 14:52:47 +0500

    simple values

---
+++ 2-simple-values/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simle values
+	// integers
+	fmt.Println(1 + 7)
+	
+	// string
+	fmt.Println("Hello, World!")
+
+	// boolean bool
+	fmt.Println(true)
+
+	// float
+	fmt.Println(3.14)
+	fmt.Println(3.14/2)
+
+
+
+
+}
